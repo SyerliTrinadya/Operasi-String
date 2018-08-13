cd2a236cb22595d1594dfd558ad02b70aa2d6f40
User <User@LAPTOP-NNIHKQG5>
2018-08-13 20:31:13 +0700

    Coba method format

--- src/cobaoperasistring/CobaOperasiString.java
+++ src/cobaoperasistring/CobaOperasiString.java
@@ -1,6 +1,8 @@
 
 package cobaoperasistring;
 
+import java.util.Locale;
+
 public class CobaOperasiString {
     public static void main(String[] args) {
         String identitas = "Syerli Trinadya / X RPL 2 / 33";
@@ -75,7 +77,14 @@ public class CobaOperasiString {
         for(int i=0;i<arrTr.length;i++)
             System.out.println(arrTr[i]);
         
+        double d = 1545454000;
+        System.out.println(String.format("Uang saya %,.4f", d));
+        
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"),
+                "Uang saya Rp %,.2f", d));
 
+
+        
 
     }
     
